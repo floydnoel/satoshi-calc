33370e90a243b79d001f403b627816750846a5c3
Floyd Noel <floyd.noel@gmail.com>
2013-11-24 11:18:34 -0700

    argument parsing now done by argparse instead of sys

--- satoshicalculator.py
+++ satoshicalculator.py
@@ -1,12 +1,12 @@
-import urllib2, json, sys
+import urllib2, json, argparse
 
-if(len(sys.argv) != 2):
-	print "Usage: satoshicalculator.py [USD_amount]"
-	sys.exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument("amount", help="amount to calculate in USD", type=float)
+args = parser.parse_args()
 
 price_url = "http://api.coindesk.com/v1/bpi/currentprice.json"
 bpi_data = json.load(urllib2.urlopen(price_url))
-input_amt = sys.argv[1]
-satoshi_amt = int((float(input_amt)) / float((bpi_data.get("bpi").get("USD").get("rate"))) * 100000000)
+input_amt = args.amount
+satoshi_amt = input_amt / float((bpi_data.get("bpi").get("USD").get("rate"))) * 100000000
 
-print "${0} is equal to {1:,} satoshi".format(input_amt, satoshi_amt)
+print "${0:.2f} is equal to {1:,} satoshi".format(input_amt, int(satoshi_amt))
